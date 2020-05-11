aa4137aa94562a9de1d0c0cec2596466a38f504e
ex-lizenghui <zA231057>
2020-05-11 17:36:05 +0800

    分支2commit

--- src/main/java/com/how2java/controller/LoginController.java
+++ src/main/java/com/how2java/controller/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 //分支1拉,1add,1commit
-
+//fenzhi2 commit
 @Controller
 @RequestMapping("")
 public class LoginController {
