35291ef139009da7b406714401b59eaccc9a3b72
ex-lizenghui <zA231057>
2020-05-11 17:17:59 +0800

    第一次commit

--- src/main/java/com/how2java/controller/LoginController.java
+++ src/main/java/com/how2java/controller/LoginController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+//11第一次add
 @Controller
 @RequestMapping("")
 public class LoginController {
